adac52b4c507734c82fff24c8e4922875d65a058
K N V PHANI KUMAR <119072808+knvphanikumar@users.noreply.github.com>
2025-03-23 12:14:41 +0530

    Update

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ secret_key = "WMzp3woSuh1Q8nbezl4rZL4jMpq5BrCcM4zNkZSf"
 }
 
 resource "aws_instance" "ec2_example" {
-  ami                    = "ami-0cca134ec43cf708f"
+  ami                    = "ami-084568db4383264d4"
   instance_type          = "t2.micro"
   key_name = "phanitf"
    availability_zone = "us-east-1b"
